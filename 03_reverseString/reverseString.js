2eb73ae6f841037a590d08370dcf1536d6ebc2ac
adelguit <adeelguitoun@gmail.com>
2022-07-16 11:38:26 +0100

    Solved.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let latters = string.split("");
+  let reversedLatters = [];
+  let output = "";
 
+  for (let i = latters.length - 1; i >= 0; i--) {
+    reversedLatters.push(latters[i]);
+  }
+
+  output = reversedLatters.join("");
+  return output;
 };
 
 // Do not edit below this line
