0b8acb24cf30bf0f43853a6b08fd2fed85b50ed5
adelguit <adeelguitoun@gmail.com>
2022-07-16 18:51:27 +0100

    Solved.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = 0;
+  const startFrom = Math.min(...arguments);
+  const endsAt = Math.max(...arguments);
 
+  if (isPositiveNumber(start) && isPositiveNumber(end)) {
+    for (let i = startFrom; i <= endsAt; i++) {
+      sum += i;
+    }
+
+    return sum;
+  } else return "ERROR";
 };
 
+function isPositiveNumber(param) {
+  return typeof param == "number" && param >= 0;
+}
 // Do not edit below this line
 module.exports = sumAll;
